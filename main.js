2de5885cd70de6b3c7af089e22e32115a6f0331f
shawksly <shawksly@gmail.com>
2023-09-02 02:43:33 -0600

    U4_03 complete - Added global variables, created async function for fetching API data, integrated that function with event listeners for specific categories, and made the data load without categories when page loads.

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
-fetch('https://fakestoreapi.com/products/categories')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
+const navElectronics = document.getElementById('nav-electronics');
+const navJewelery = document.getElementById('nav-jewelery');
+const navMensClothing = document.getElementById('nav-mens-clothing');
+const navWomensClothing = document.getElementById('nav-womens-clothing');
+const display = document.getElementById('display');
+const url = 'https://fakestoreapi.com/products';
+
+let fakeStore = async (endpoint) => {
+  let response = await fetch(endpoint);
+  let storeData = await response.json();
+
+  console.log(storeData);
+};
+
+navElectronics.addEventListener('click', e => {
+  e.preventDefault();
+  fakeStore(url + `/category/electronics?sort=desc`);
+});
+
+navJewelery.addEventListener('click', e => {
+  e.preventDefault();
+  fakeStore(url + `/category/jewelery?sort=desc`);
+});
+
+navMensClothing.addEventListener('click', e => {
+  e.preventDefault();
+  fakeStore(url + `/category/men's%20clothing?sort=desc`);
+});
+
+navWomensClothing.addEventListener('click', e => {
+  e.preventDefault();
+  fakeStore(url + `/category/women's%20clothing?sort=desc`);
+});
+
+window.onload = (event) => {
+  fakeStore(url + `?sort=desc`);
+};
