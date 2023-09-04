3ea2b0429c2865e9bf07f8e59d83e5368d3a392e
shawksly <shawksly@gmail.com>
2023-09-04 05:26:48 -0600

    U4_04 Partial - Created displayCards() function, created and styled a mock card in html for guidance, added a loading spinner, started on card in js

--- main.js
+++ main.js
@@ -5,33 +5,60 @@ const navWomensClothing = document.getElementById('nav-womens-clothing');
 const display = document.getElementById('display');
 const url = 'https://fakestoreapi.com/products';
 
+function displayCards(data) {
+  
+  display.innerHTML = '';
+  // 1. create new element
+  // 2. edit the element
+  // 3. add the element to the webpage
+
+  let row = document.createElement('div');
+  row.className = 'row';
+  display.appendChild(row);
+
+  // data.forEach(item => {
+
+  // });
+
+};
+
 let fakeStore = async (endpoint) => {
+
+  let loadingContainer = document.createElement('div');
+  loadingContainer.className = 'd-flex justify-content-center'
+  display.appendChild(loadingContainer)
+  let loading = document.createElement('div');
+  loading.className = 'spinner-border m-5 text-info';
+  loading.role = 'status';
+  loadingContainer.appendChild(loading);
+
   let response = await fetch(endpoint);
   let storeData = await response.json();
 
   console.log(storeData);
+  displayCards(storeData);
 };
 
 navElectronics.addEventListener('click', e => {
   e.preventDefault();
-  fakeStore(url + `/category/electronics?sort=desc`);
+  fakeStore(url + `/category/electronics?sort=asc`);
 });
 
 navJewelery.addEventListener('click', e => {
   e.preventDefault();
-  fakeStore(url + `/category/jewelery?sort=desc`);
+  fakeStore(url + `/category/jewelery?sort=asc`);
 });
 
 navMensClothing.addEventListener('click', e => {
   e.preventDefault();
-  fakeStore(url + `/category/men's%20clothing?sort=desc`);
+  fakeStore(url + `/category/men's%20clothing?sort=asc`);
 });
 
 navWomensClothing.addEventListener('click', e => {
   e.preventDefault();
-  fakeStore(url + `/category/women's%20clothing?sort=desc`);
+  fakeStore(url + `/category/women's%20clothing?sort=asc`);
 });
 
 window.onload = (event) => {
-  fakeStore(url + `?sort=desc`);
+  fakeStore(url + `?sort=asc`);
 };
